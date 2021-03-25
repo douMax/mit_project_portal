b75433d36b6fb65c52a8461972caca375f60ed87
Atz18 <75229482+Atz18@users.noreply.github.com>
2021-03-25 14:35:10 +1100

    Student and Staff Sign Up pages.
     - Modified App.js to allow for basic navigation to sign up pages.
    - Added Student Sign Up page.
    - Added Staff Sign Up page.
    - Both pages share common components.
    - Added option for profile picture upload (not yet tested).
    - Both pages are mostly static at the moment.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
+import SignUp from "./components/SignupPage/";
 
-let isLogged = false;
+let isLogged = true;
+let isSignedUp = false;
 
 function App() {
-  return isLogged ? (
-    <div>
-      <Header />
-      <div>APP content</div>
-    </div>
-  ) : (
-    <LandingPage />
-  );
+
+  if (isLogged === true && isSignedUp === true) {
+    return (
+      <dir>
+        <Header />
+        <h3>APP Content</h3>
+      </dir>
+    )
+  }
+  else if (isLogged === true && isSignedUp === false) {
+    return (
+      <SignUp />
+    )
+  }
+  else {
+    return (
+      <LandingPage />
+    )
+  }
 }
 
 export default App;
---
+++ frontend/src/components/SignupPage/index.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import StudentSignUp from './StudentSignUp';
+import StaffSignUp from './StaffSignUp';
+import ProfilePicUpload from './ProfilePicUpload';
+
+import {
+  Form,
+  Input,
+  Button,
+  Radio,
+  Space,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+} from 'antd';
+
+let isStudent = false;
+
+const SignUp = () => {
+
+    const tailLayout = {
+        wrapperCol: {
+          offset: 10,
+          span: 16
+        },
+      };
+
+    return (
+    <div className="signup-page">
+        <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 9,
+        }}
+        layout="horizontal"
+        >
+
+        {isStudent ? <div className="page-title">Student Sign Up</div> : <div className="page-title">Staff Sign Up</div>}  
+        
+        <Form.Item label="Title">
+            <Select>
+                <Select.Option value="Miss">Miss</Select.Option>
+                <Select.Option value="Mr">Mr</Select.Option>
+                <Select.Option value="Mrs">Mrs</Select.Option>
+                <Select.Option value="Dr">Dr</Select.Option>
+                <Select.Option value="Prof">Prof</Select.Option>
+                <Select.Option value="A.Prof">A.Prof</Select.Option>
+            </Select>
+        </Form.Item>
+        <Form.Item label="First Name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Last Name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="User Name">
+          <Input />
+        </Form.Item>
+        <ProfilePicUpload />
+        <Form.Item label="MIT Email Address">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Personal Email Address">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Mobile Phone Number">
+          <Input />
+        </Form.Item>
+        {isStudent ? <StudentSignUp /> : <StaffSignUp />}
+        <Form.Item {...tailLayout}>
+            <Space>
+                <Button danger htmlType="button">
+                    Cancel
+                </Button>
+                <Button type="danger" htmlType="submit">
+                    Submit
+                </Button>
+            </Space>
+        </Form.Item>
+        </Form>
+    </div>
+    );
+};
+
+export default SignUp;
