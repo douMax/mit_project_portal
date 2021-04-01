eb031169ce046facb4dd743b2958a3186ffa78b3
QiZhao8 <zhaozuozhen2018@gmail.com>
2021-04-01 16:24:22 +1100

    updated indusstry client form

---
+++ frontend/src/components/Industryclient/index.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import ClientSignupForm from "./ClientSignupForm";
+
+const ClientSignUp = () => {
+  return (
+    <div>
+      <ClientSignupForm />
+    </div>
+  );
+};
+
+export default ClientSignUp;
