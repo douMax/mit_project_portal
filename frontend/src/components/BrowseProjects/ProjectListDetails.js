d5e8fb7a9d724dcf375f3ef7a69aa95b24f09fa1
Atz18 <75229482+Atz18@users.noreply.github.com>
2021-04-05 10:09:37 +1000

    Adding the browse project case.

---
+++ frontend/src/components/BrowseProjects/ProjectListDetails.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Layout, Card, Select, Button, Space } from "antd";
+
+const { Content, Sider } = Layout;
+const { Option } = Select;
+
+const TopicsHeader = styled.nav`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 14px;
+  color: #ff4d4f;
+`;
+
+const ProjectTitle = styled.nav`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  padding-bottom: 5px;
+`;
+
+const ProjectDescription = styled.nav`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 14px;
+  padding-bottom: 10px;
+`;
+
+const ClientName = styled.nav`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 14px;
+`;
+
+let isClicked = false;
+
+const ProjectListDetails = ({
+  projId,
+  title,
+  topic,
+  description,
+  status,
+  year,
+  trimester,
+  assigned_students,
+  eoi,
+  client,
+  logo,
+  background_rationale,
+  resources,
+  goals_objectives,
+}) => {
+  const changeBorderRed = () => {
+    isClicked
+      ? (document.getElementById(projId).style.borderColor = "red")
+      : (document.getElementById(projId).style.borderColor = "white");
+    isClicked = !isClicked;
+  };
+
+  return (
+    <Layout key={projId}>
+      <Sider
+        style={{
+          background: "#f0f0f0",
+          paddingLeft: "30px",
+          paddingRight: "10px",
+          paddingBottom: "30px",
+        }}
+        width={660}
+      >
+        <Card
+          id={projId}
+          style={{
+            width: 620,
+            height: 180,
+          }}
+          hoverable="true"
+          onClick={changeBorderRed}
+        >
+          <Space>
+            <Content style={{ width: 450 }}>
+              <TopicsHeader>{topic}</TopicsHeader>
+              <ProjectTitle>{title}</ProjectTitle>
+              <ProjectDescription>{description}</ProjectDescription>
+              <Space>
+                <Button style={{ background: "turquoise", color: "white" }}>
+                  {status}
+                </Button>
+                <Button type="text">
+                  {year} - T{trimester}
+                </Button>
+                <Button type="text">Group: {assigned_students}/5</Button>
+                <Button type="text">EOIs: {eoi}</Button>
+              </Space>
+            </Content>
+            <ClientName>
+              <img src={logo} alt={client} width="" height="" />
+              {client}
+            </ClientName>
+          </Space>
+        </Card>
+      </Sider>
+      {/*<Content
+        style={{
+          background: "#f0f0f0",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+        }}
+        width={710}
+      >
+        <Card style={{ width: 670, height: 150 }}></Card>
+      </Content>*/}
+    </Layout>
+  );
+};
+
+export default ProjectListDetails;
--- frontend/src/components/BrowseProjects/index.js
+++ frontend/src/components/BrowseProjects/index.js
@@ -1,22 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Layout } from "antd";
 
-const { Content, Sider } = Layout;
+import ProjectListDetails from "./ProjectListDetails";
+import SearchNSort from "./SearchNSort";
 
 const PageTitle = styled.nav`
-  margin-left: 20px;
+  margin-left: 30px;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   font-size: 24px;
   font-weight: bold;
   letter-spacing: 2px;
+  background-color: #f0f0f0;
 `;
 
 const BrowseProjects = () => {
+  const [projects, setProjects] = useState([
+    {
+      projId: "1",
+      title: "Automated Web Portal for Capstone Project",
+      topic: "Web, Cybersecurity, Data Analytics",
+      description:
+        "Build an automated web portal for managing students’ capstone projects for MIT. Implement the project proposal review process.",
+      status: "Open",
+      year: "2021",
+      trimester: "1",
+      assigned_students: 0,
+      eoi: 12,
+      client: "MIT",
+      logo: "client_logo_mit.JPG",
+      background_rationale:
+        "Background and Rationale for Automated Web Portal for Capstone Project.",
+      resources:
+        "Project Resources for Automated Web Portal for Capstone Project.",
+      goals_objectives:
+        "Goals and Objectives for Automated Web Portal for Capstone Project.",
+    },
+
+    {
+      projId: "2",
+      title: "SmartList - A solid replacement of SmartFridges",
+      topic: "IoT, Mobile App, Cybersecurity",
+      description:
+        "Build a SmartList mobile app that interactes with fridge using wireless sensor and provides shopping list functioanlities.",
+      status: "Open",
+      year: "2021",
+      trimester: "1",
+      assigned_students: 2,
+      eoi: 24,
+      client: "Arif Systems",
+      logo: "client_logo_arifsystems.JPG",
+      background_rationale:
+        "Background and Rationale for SmartList - A solid replacement of SmartFridges.",
+      resources:
+        "Project Resources for SmartList - A solid replacement of SmartFridges.",
+      goals_objectives:
+        "Goals and Objectives for SmartList - A solid replacement of SmartFridges.",
+    },
+
+    {
+      projId: "3",
+      title: "Instawebworks - Python-based Data Scraping",
+      topic: "Data Science, Python, Data Analytics",
+      description:
+        "Using Twitter API to scraping data and use semantic ananylsis API to perform sentiment evaluation for business intelligence.",
+      status: "Open",
+      year: "2021",
+      trimester: "1",
+      assigned_students: 4,
+      eoi: 24,
+      client: "Arif Systems",
+      logo: "client_logo_arifsystems.JPG",
+      background_rationale:
+        "Background and Rationale for Instawebworks - Python-based Data Scraping.",
+      resources:
+        "Project Resources for Instawebworks - Python-based Data Scraping.",
+      goals_objectives:
+        "Goals and Objectives for Instawebworks - Python-based Data Scraping.",
+    },
+  ]);
+
   return (
     <Layout>
       <PageTitle>Browse Projects</PageTitle>
+      <SearchNSort />
+      <Layout style={{ background: "#f0f0f0" }}>
+        {projects.map((project) => (
+          <ProjectListDetails
+            projId={project.projId}
+            title={project.title}
+            topic={project.topic}
+            description={project.description}
+            status={project.status}
+            year={project.year}
+            trimester={project.trimester}
+            assigned_students={project.assigned_students}
+            eoi={project.eoi}
+            client={project.client}
+            logo={project.logo}
+            background_rationale={project.background_rationale}
+            resources={project.resources}
+            goals_objectives={project.goals_objectives}
+          />
+        ))}
+      </Layout>
     </Layout>
   );
 };
