42b671e70940fe3c2970582bc25b601422edae33
Atz18 <75229482+Atz18@users.noreply.github.com>
2021-05-11 13:28:36 +1000

    Updates to the Coordinator Dashboard to display student details when Allocating projects

--- frontend/src/components/CoordinatorDashboard/StudentEOIDetail.js
+++ frontend/src/components/CoordinatorDashboard/StudentEOIDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Space, Input } from "antd";
+import { Card, Space, Input, Button, Switch } from "antd";
 import styled from "styled-components";
 
 const { TextArea } = Input;
@@ -9,36 +9,56 @@ const SectionTitle = styled.h1`
   font-weight: bold;
 `;
 
-const EOIDetailsSubsection = styled.p`
+const SubsectionTitle = styled.p`
   font-size: 12px;
   font-weight: bold;
 `;
 
 const StudentEOIDetails = ({ eoi }) => {
+  //Need to query database to get student details based on studentId obtained from the EOI object.
   return (
     <Card>
       <Space>
-        <Card>
-          <SectionTitle>Student Name</SectionTitle>
-          <SectionTitle>Enrolment Status</SectionTitle>
+        <Card style={{ height: 385 }}>
+          <SubsectionTitle>Lingxiao Wang</SubsectionTitle>
+          <Button type="text" style={{ background: "turquoise" }}>
+            Allocate
+          </Button>
+          <Button type="primary" danger style={{ marginTop: 10 }}>
+            Remove
+          </Button>
+          <SubsectionTitle>Course Enrolment Status</SubsectionTitle>
+          <Switch
+            checkedChildren="Yes"
+            unCheckedChildren="No"
+            defaultChecked
+            disabled
+          />
+          <SubsectionTitle>Module Enrolment Status</SubsectionTitle>
+          <Switch
+            checkedChildren="Yes"
+            unCheckedChildren="No"
+            defaultChecked
+            disabled
+          />
         </Card>
         <Card>
           <SectionTitle>Expression of Interest Details</SectionTitle>
-          <EOIDetailsSubsection>Interest in Project</EOIDetailsSubsection>
+          <SubsectionTitle>Interest in Project</SubsectionTitle>
           <TextArea
             cols={60}
             style={{ background: "white", color: "black" }}
             disabled
             defaultValue={eoi.interest}
           ></TextArea>
-          <EOIDetailsSubsection>Achievement Goals</EOIDetailsSubsection>
+          <SubsectionTitle>Achievement Goals</SubsectionTitle>
           <TextArea
             cols={60}
             style={{ background: "white", color: "black" }}
             disabled
             defaultValue={eoi.achieve}
           ></TextArea>
-          <EOIDetailsSubsection>Previous Experience</EOIDetailsSubsection>
+          <SubsectionTitle>Previous Experience</SubsectionTitle>
           <TextArea
             cols={60}
             style={{ background: "white", color: "black" }}
